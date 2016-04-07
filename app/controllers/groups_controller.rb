d953547b098bebb08ae345a199d2cf2a0132cee4
twst912006 <twst912006@gmail.com>
2016-04-07 17:29:00 +0800

    delete system finished

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,50 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.create(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @posts = @group.posts 
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to group_path, notice: "修改完畢。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "刪除完畢。"
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end
